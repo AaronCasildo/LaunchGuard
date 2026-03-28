b6be7326933b17faf8b86ba7c0091f450002aefd
AaronCasildo <aacasildor@gmail.com>
2026-03-27 21:38:49 -0700

    chore: add icon to main ui

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Security.Permissions;
 using System.Windows.Forms;
 
 namespace LaunchGuard;
@@ -18,11 +19,12 @@ internal sealed class MainForm : Form
 {
     public MainForm()
     {
+        // Form properties
         Text = "LaunchGuard";
         StartPosition = FormStartPosition.CenterScreen;
         ClientSize = new Size(640, 360);
         FormBorderStyle = FormBorderStyle.FixedSingle;
         MaximizeBox = false;
-    
+        Icon = new Icon("media\\lock.ico");
     }
 }
