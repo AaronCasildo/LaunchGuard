53e5fd62a8645396b5f21ad3e8c2aa2b7d29a9b6
AaronCasildo <aacasildor@gmail.com>
2026-03-27 21:32:08 -0700

    feat: update main file with a white windows forms template

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LaunchGuard;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
+
+internal sealed class MainForm : Form
+{
+    public MainForm()
+    {
+        Text = "LaunchGuard";
+        StartPosition = FormStartPosition.CenterScreen;
+        ClientSize = new Size(640, 360);
+        FormBorderStyle = FormBorderStyle.FixedSingle;
+        MaximizeBox = false;
+    
+    }
+}
