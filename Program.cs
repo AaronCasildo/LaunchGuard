4ca923950e9e58efdce75b5ea6ae0e02aa85b02c
AaronCasildo <aacasildor@gmail.com>
2026-03-28 21:07:13 -0700

    feat: add listbox to display process initialization events, add welcome message

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Security.Permissions;
 using System.Windows.Forms;
 using System.Management;
+using System.Drawing.Text;
 
 namespace LaunchGuard;
 
@@ -28,6 +29,22 @@ internal sealed class MainForm : Form
         MaximizeBox = false;
         Icon = new Icon("media\\lock.ico");
 
+        Label welcomeLabel = new Label()
+        {
+            Text = "Welcome to LaunchGuard!",
+            Font = new Font("Segoe UI", 10, FontStyle.Bold),
+            AutoSize = true,
+            Location = new Point(20, 20)
+        };
+        Controls.Add(welcomeLabel);
+
+        ListBox processListBox = new ListBox()
+        {
+            Location = new Point(20, 60),
+            Size = new Size(600, 200),
+        };  
+        Controls.Add(processListBox);
+
         //Software initialization watcher
         var query = new WqlEventQuery(
             "__InstanceCreationEvent",
@@ -36,13 +53,22 @@ internal sealed class MainForm : Form
         );
 
         var watcher = new ManagementEventWatcher(query);
-
         watcher.EventArrived += (sender,e) =>
         {
             var process = (ManagementBaseObject)e.NewEvent["TargetInstance"];
-            MessageBox.Show("System initialized:" + process["Name"], "LaunchGuard", 
-            MessageBoxButtons.OK, MessageBoxIcon.Information);
+            var processName = process["Name"]?.ToString() ?? "Unknown";
+
+            BeginInvoke(() =>
+            {
+                newEvent($"System initialized: {processName}");
+            });
         };
+
+        void newEvent(string message)
+        {
+            processListBox.Items.Insert(0, message);
+            processListBox.SelectedIndex = 0;
+        }
 
         watcher.Start(); //duh, 5 min wondering why it didn't work hahahh
         
