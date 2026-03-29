64d92e23d765b76f6adf1947350904cfe5508ab3
AaronCasildo <aacasildor@gmail.com>
2026-03-28 22:10:46 -0700

    feat: add datagridview tool for process name and password setting

--- Settings.cs
+++ Settings.cs
@@ -17,6 +17,20 @@ internal sealed class SettingsForm : Form
         ControlBox = true;
         Icon = new Icon("media\\lock.ico");
 
+        DataGridView settingsGrid = new DataGridView()
+        {
+            Location = new Point(20, 20),
+            Size = new Size(560, 180),
+            AllowUserToAddRows = true,
+            AllowUserToDeleteRows = true,
+            ReadOnly = false,
+            ColumnCount = 2
+        };  
+        Controls.Add(settingsGrid);
 
+        settingsGrid.Columns[0].Name = "Software process name";
+        settingsGrid.Columns[1].Name = "Password";
+        settingsGrid.Columns[0].Width = 260;
+        settingsGrid.Columns[1].Width = 255;
     }
 }
