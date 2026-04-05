a6ebf9496e42df26a72d5fa99b7b983d1f0958b3
AaronCasildo <aacasildor@gmail.com>
2026-04-04 19:04:12 -0700

    feat: add screen dpi handling

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ internal static class Program
     [STAThread]
     static void Main()
     {
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
         ApplicationConfiguration.Initialize();
         AppConfig.Load();
         Application.Run(new MainForm());
